c4d84b5ca0b13bc9572de109d77cf6db2ca0f8a7
Shubham Nanche <shubhamnanche19@gmail.com>
2024-12-20 23:01:45 +0530

    implement stateless auth using jwt tokens

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "A42hzuat27";
 
-function setUser(id, user) {
-  sessionIdToUserMap[id] = user;
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap[id];
+function getUser(token) {
+  if (!token) {
+    return null;
+  }
+  try {
+    return jwt.verify(token, secret);
+  } catch (e) {
+    return null;
+  }
 }
 
 module.exports = {
